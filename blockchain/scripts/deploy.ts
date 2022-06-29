dac6ac60c2ce909798ea24fdc9d16c62c29a3011
romfol1 <roman.foltyn@espeo.eu>
2022-06-29 20:48:56 +0300

    cleanup

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,28 +1,28 @@
 import { ethers, run } from "hardhat";
 
 async function main() {
-  // const NFToken = await ethers.getContractFactory("NFToken");
-  // console.log("Deploying NFToken ERC721 token...");
-  // const token = await NFToken.deploy("Vasily Ivanovich", "VI721");
+  const NFToken = await ethers.getContractFactory("NFToken");
+  console.log("Deploying NFToken ERC721 token...");
+  const token = await NFToken.deploy("NFToken", "ERC721");
 
-  // await token.deployed();
-  // console.log("NFToken deployed to:", token.address);
-
-  // await run("verify", {
-  //   address: "0xc57bcFf78d8f5E5c765CBE122da4Cdd206C79D1E",
-  //   contract: "contracts/NFToken.sol:NFToken",
-  //   constructorArguments: ["Vasily Ivanovich", "VI721"],
-  // });
-
-  // const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
-  // console.log("Deploying NFTMarketplace...");
-  // const marketplace = await NFTMarketplace.deploy();
-
-  // await marketplace.deployed();
-  // console.log("NFTMarketplace deployed to:", marketplace.address);
+  await token.deployed();
+  console.log("NFToken deployed to:", token.address);
 
   await run("verify", {
-    address: "0xB4a9e712394633Abba8CC9F801FE8E363671bb60",
+    address: token.address,
+    contract: "contracts/NFToken.sol:NFToken",
+    constructorArguments: ["NFToken", "ERC721"],
+  });
+
+  const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+  console.log("Deploying NFTMarketplace...");
+  const marketplace = await NFTMarketplace.deploy();
+
+  await marketplace.deployed();
+  console.log("NFTMarketplace deployed to:", marketplace.address);
+
+  await run("verify", {
+    address: marketplace.address,
     contract: "contracts/NFTMarketplace.sol:NFTMarketplace",
   });
 }
